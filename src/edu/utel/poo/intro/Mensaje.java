70aa758fa73bb4c88e8c1b64b9a9af99c1f6b462
atellezf <alejandro@correo.com>
2024-01-12 21:43:22 -0600

    Código de la OpenClass S1

---
+++ src/edu/utel/poo/intro/Mensaje.java
@@ -0,0 +1,9 @@
+package edu.utel.poo.intro;
+
+public class Mensaje {
+
+    public void saludar(String nombre) {
+        System.out.println("Hola " + nombre + ", bienvenido a Java");
+    }
+
+}
---
+++ src/edu/utel/poo/intro/Programa.java
@@ -0,0 +1,10 @@
+package edu.utel.poo.intro;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        Mensaje msg = new Mensaje();
+        msg.saludar("Benito Bodoque");
+    }
+
+}
